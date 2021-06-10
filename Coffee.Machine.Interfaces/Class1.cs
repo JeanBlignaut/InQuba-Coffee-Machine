ce31310d988d12afb0d301b4db82d9f800207eb7
jean <jean.blignaut@me.com>
2021-06-10 20:57:13 +0200

    first commit

---
+++ Coffee.Machine.Interfaces/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Coffee.Machine.Interfaces
+{
+    public class Class1
+    {
+    }
+}
---
+++ Coffee.Machine.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Coffee.Machine.Logic
+{
+    public class Class1
+    {
+    }
+}
